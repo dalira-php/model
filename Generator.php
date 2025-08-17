e621f8602f9e8a261f167b6dd98d9d79238fff29
ONESYSTEAM <info.onesysteam@gmail.com>
2025-08-17 19:27:40 +0800

    Updated

--- Generator.php
+++ Generator.php
@@ -12,36 +12,16 @@ $modelContent = <<<PHP
 
 namespace app\Models;
 
-use PDO;
 use config\DBConnection;
+use PDO;
+use PDOException
 
-/**
- * Class $modelName
- *
- * This class provides an abstraction layer for interacting with the database 
- * related to the '$modelName' model. It encapsulates database operations, 
- * ensuring a clean and maintainable interface for managing data.
- *
- * @package app\Models
- */
 class $modelName
 {
-    /**
-     * @var \PDO Database connection instance.
-     */
     private \$db;
 
-    /**
-     * Constructor.
-     *
-     * Initializes the database connection.
-     *
-     * @param DBConnection \$db An instance of DBConnection for establishing 
-     * the database connection.
-     */
     public function __construct(DBConnection \$db)
     {
-        // Retrieve the database connection from DBConnection
         \$this->db = \$db->getConnection();
     }
     
